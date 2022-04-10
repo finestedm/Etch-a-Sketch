033c8d78803b3cab27adf13acdebed5290be2301
pawel <finestedm@gmail.com>
2022-04-10 18:30:15 +0200

    working, unstyled version

---
+++ myscript.js
@@ -0,0 +1,58 @@
+const newArray = (16 ** 2);
+var currentColor = '';
+
+// This part created an array of boxes - size depends on newArray value
+for (i = 0; i < newArray; i++) {
+    var div = document.createElement('div');
+    div.setAttribute("id", "box-" + i);
+    div.setAttribute("class", "box");
+    container.appendChild(div);
+}
+
+const box = document.querySelectorAll('.box');
+const color = document.querySelectorAll('#color-chooser button');
+const reset = document.querySelector('#reset');
+
+
+// This part colors the clicked box to the chosen color
+box.forEach((box) => {
+    box.addEventListener('mousedown', (e) => {
+        e.target.style.background = currentColor;
+    });
+});
+
+// This part assignes the clicked color as a current color as well as sets a background color of color-choosing buttons
+color.forEach((color) => {
+    color.addEventListener('click', (e) => {
+        chosenColor = (e.target.innerHTML)
+        currentColor = chosenColor;
+        resetAllButtonsBackground();
+        e.target.setAttribute('class', `pressed`);
+        e.target.style.backgroundColor = `${currentColor}`;
+    });
+});
+
+// This part clears background color off color-choosing buttons
+function resetAllButtonsBackground() {
+    color.forEach((color) => {
+        color.setAttribute('class', 'unpressed')
+        color.style.backgroundColor = ``;
+    });
+}
+
+reset.addEventListener('mousedown', () => {
+    resetEverything();
+});
+
+function resetEverything() {
+    color.forEach((color) => {
+        color.setAttribute('class', 'unpressed')
+        color.style.backgroundColor = ``;
+    });
+
+    for (i = 0; i < newArray; i++) {
+        currentBoxId = 'box-' + `${i}`;
+        currentBox = document.getElementById(currentBoxId);
+        currentBox.style.backgroundColor = ``;
+    }
+}
